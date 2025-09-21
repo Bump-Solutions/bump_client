9d9b7420583f860ae9a2a9ff72470e0b3837860c
Richárd Bene <bene.richard24@gmail.com>
2025-09-21 20:06:49 +0200

    CartContextMenu, clearCart

--- src/modules/cart/CartContextMenu.tsx
+++ src/modules/cart/CartContextMenu.tsx
@@ -3,12 +3,14 @@ import { useCart } from "../../hooks/cart/useCart";
 import { motion } from "framer-motion";
 import { useClickOutside } from "../../hooks/useClickOutside";
 
+import { ArrowUpRight } from "lucide-react";
+
 interface CartContextMenuProps {
   toggleContextMenu: (value?: boolean) => void;
 }
 
 const CartContextMenu = ({ toggleContextMenu }: CartContextMenuProps) => {
-  const { cart } = useCart();
+  const { cart, clearCart } = useCart();
 
   const ref = useRef<HTMLDivElement>(null);
 
@@ -27,7 +29,29 @@ const CartContextMenu = ({ toggleContextMenu }: CartContextMenuProps) => {
       transition={{
         duration: 0.2,
         ease: [0.25, 0.46, 0.45, 0.94],
-      }}></motion.div>
+      }}>
+      <ul className='action-list'>
+        <li className='action-list-item blue icon-end'>
+          <div>
+            <ArrowUpRight />
+            <span>Megosztás</span>
+          </div>
+        </li>
+      </ul>
+
+      <ul className='action-list no-border'>
+        <li className='action-list-item red '>
+          <div
+            onClick={(e) => {
+              e.preventDefault();
+              clearCart();
+              toggleContextMenu(false);
+            }}>
+            <span>Kosár ürítése</span>
+          </div>
+        </li>
+      </ul>
+    </motion.div>
   );
 };
 
--- src/modules/cart/CartHeader.tsx
+++ src/modules/cart/CartHeader.tsx
@@ -1,10 +1,11 @@
-import { useDeferredValue, useRef } from "react";
+import { useDeferredValue, useEffect, useRef } from "react";
 import { useCart } from "../../hooks/cart/useCart";
 import { useToggle } from "../../hooks/useToggle";
 import { AnimatePresence } from "framer-motion";
 
-import { EllipsisVertical, Search } from "lucide-react";
 import CartContextMenu from "./CartContextMenu";
+
+import { EllipsisVertical, Search } from "lucide-react";
 
 interface CartHeaderProps {
   searchKey: string;
@@ -20,50 +21,56 @@ const CartHeader = ({ searchKey, setSearchKey }: CartHeaderProps) => {
 
   const [isContextMenuOpen, toggleContextMenu] = useToggle(false);
 
+  useEffect(() => {
+    document.body.style.overflow = isContextMenuOpen ? "hidden" : "auto";
+    document.body.style.pointerEvents = isContextMenuOpen ? "none" : "auto";
+
+    return () => {
+      document.body.style.overflow = "auto";
+      document.body.style.pointerEvents = "auto";
+    };
+  }, [isContextMenuOpen]);
+
   return (
-    <>
-      <header className='cart__header'>
-        <div>
-          <h1>A Te kosarad</h1>
-          {cart.summary.itemsCount > 0 && (
-            <span className='badge fw-600'>
-              {cart.summary.itemsCount} tétel
-            </span>
-          )}
+    <header className='cart__header'>
+      <div>
+        <h1>A Te kosarad</h1>
+        {cart.summary.itemsCount > 0 && (
+          <span className='badge fw-600'>{cart.summary.itemsCount} tétel</span>
+        )}
+      </div>
+      <div>
+        <div
+          className='search-box'
+          role='search'
+          onClick={() => searchRef.current?.focus()}>
+          <Search aria-hidden='true' />
+          <input
+            type='search'
+            className='form-control'
+            placeholder='Keresés...'
+            value={deferredSearchKey}
+            onChange={(e) => setSearchKey(e.target.value)}
+            ref={searchRef}
+          />
         </div>
-        <div>
-          <div
-            className='search-box'
-            role='search'
-            onClick={() => searchRef.current?.focus()}>
-            <Search aria-hidden='true' />
-            <input
-              type='search'
-              className='form-control'
-              placeholder='Keresés...'
-              value={deferredSearchKey}
-              onChange={(e) => setSearchKey(e.target.value)}
-              ref={searchRef}
-            />
-          </div>
-          <span
-            className='cart-actions'
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              toggleContextMenu(true);
-            }}>
-            <EllipsisVertical strokeWidth={3} />
-          </span>
-        </div>
-      </header>
+        <span
+          className='cart-actions'
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            toggleContextMenu(true);
+          }}>
+          <EllipsisVertical strokeWidth={3} />
+        </span>
+      </div>
 
       {isContextMenuOpen && (
         <AnimatePresence mode='wait'>
           <CartContextMenu toggleContextMenu={toggleContextMenu} />
         </AnimatePresence>
       )}
-    </>
+    </header>
   );
 };
 
